0bcbc81545e360e763bbc72377f0183c10da3cde
Pawel <pawel2antonik@gmail.com>
2019-12-30 15:33:18 +0100

    add rules

--- main.go
+++ main.go
@@ -6,9 +6,26 @@ import (
 	"net/http"
 )
 
+type Answer struct {
+	Direction string
+}
+
+type Data struct {
+	Board [][]int
+}
+
 func main() {
-	http.HandleFunc("/load", onLoad)
-	http.ListenAndServe(":8080", nil)
+	// http.HandleFunc("/load", onLoad)
+	// http.ListenAndServe(":8080", nil)
+
+	var newGrid = [][]int{
+		{0, 0, 0, 0},
+		{0, 4, 0, 0},
+		{0, 2, 2, 0},
+		{0, 0, 0, 0},
+	  }
+	var grid = move(newGrid, "RIGHT")
+	fmt.Println(grid)
 }
 
 func onLoad(w http.ResponseWriter, r *http.Request) {
@@ -17,21 +34,106 @@ func onLoad(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-	name := r.FormValue("name")
-	fmt.Fprintf(w, "Name = %s\n", name)
+	boardJson := r.FormValue("board")
+	fmt.Println(boardJson)
 
-	answer := Answer{Direction: "LEFT"}
+	var data Data
+	err := json.Unmarshal([]byte(boardJson), &data)
+
+	if err != nil {
+		fmt.Fprintln(w, err.Error())
+	}
+
+	bestDirection := getBestDirection(data.Board)
+
+	answer := Answer{Direction: bestDirection}
 	b, err := json.Marshal(answer)
 
 	if err != nil {
-		fmt.Println("ERROR")
+		fmt.Fprintln(w, err.Error())
 	} else {
 		fmt.Fprintln(w, string(b))
 		fmt.Println(string(b))
 	}
 }
 
-type Answer struct {
-	Direction string
+func getBestDirection(board [][]int) string {
+	return "RIGHT"
 }
+
+func move(passedBoard [][]int, direction string) [][]int {
+    var actualBoard [][]int
+    if (direction == "DOWN") {
+      actualBoard = passedBoard;
+    } else if (direction == "RIGHT") {
+      actualBoard = rotateTableLeft(passedBoard);
+      actualBoard = rotateTableLeft(actualBoard);
+      actualBoard = rotateTableLeft(actualBoard);
+    } else if (direction == "UP") {
+      actualBoard = rotateTableLeft(passedBoard);
+      actualBoard = rotateTableLeft(actualBoard);
+    } else if (direction == "LEFT") {
+      actualBoard = rotateTableLeft(passedBoard);
+    }
+
+    for column := 0; column < 4; column++ {
+      _moveDown(actualBoard, column);
+      _join(actualBoard, column);
+      _moveDown(actualBoard, column);
+    }
+
+	var resultBoard [][]int
+    if (direction == "DOWN") {
+      resultBoard = actualBoard;
+    } else if (direction == "RIGHT") {
+      resultBoard = rotateTableLeft(actualBoard);
+    } else if (direction == "UP") {
+      resultBoard = rotateTableLeft(actualBoard);
+      resultBoard = rotateTableLeft(resultBoard);
+    } else if (direction == "LEFT") {
+      resultBoard = rotateTableLeft(actualBoard);
+      resultBoard = rotateTableLeft(resultBoard);
+      resultBoard = rotateTableLeft(resultBoard);
+    }
+
+    return resultBoard;
+  }
+
+  func _moveDown(actualBoard [][]int, column int) {
+    for times := 0; times < 3; times++ {
+      for row := 3; row > 0; row-- {
+        if (actualBoard[row][column] == 0) {
+          actualBoard[row][column] = actualBoard[row - 1][column];
+          actualBoard[row - 1][column] = 0;
+        }
+      }
+    }
+  }
+
+  func _join(actualBoard [][]int, column int) {
+    for row := 3; row > 0; row-- {
+      var value = actualBoard[row][column];
+      var valueUp = actualBoard[row - 1][column];
+      if (value == valueUp) {
+        actualBoard[row][column] = actualBoard[row - 1][column] * 2;
+        actualBoard[row - 1][column] = 0;
+      }
+    }
+  }
+
+  func rotateTableLeft(board [][]int) [][]int{
+    var newGrid = [][]int{
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+	}
+    for i := 0; i < 4; i++ {
+      for j := 0; j < 4; j++ {
+        var value = board[i][j];
+
+        newGrid[3 - j][i] = value;
+      }
+    }
+    return newGrid;
+  }
